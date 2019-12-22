0c81230f2fe426123cc6d1111545023ed48657ab
DebarunMitra <debarun96mitra@gmail.com>
2019-12-22 14:48:13 +0530

    interfaces

---
+++ ProjectOne/js/interfaces.js
@@ -0,0 +1,12 @@
+// function showTodo(todo:{title:string, text: string}){
+//   console.log(todo.title+': '+todo.text);
+// }
+//
+// let myTodo={title:'Debarun', text:'I am a software Developer'};
+//
+// showTodo(myTodo);
+function showTodo(todo) {
+    console.log(todo.title + ': ' + todo.text);
+}
+var myTodo = { title: 'Debarun', text: 'I am a software Developer' };
+showTodo(myTodo);
