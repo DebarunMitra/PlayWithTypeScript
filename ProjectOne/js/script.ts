50000f0cc79d4b1eb2d7ff3f5b98dace37ad8345
DebarunMitra <debarun96mitra@gmail.com>
2019-12-22 12:20:45 +0530

    data types in typescript

--- ProjectOne/js/script.ts
+++ ProjectOne/js/script.ts
@@ -1,5 +1,42 @@
 let myString:String;
+let myNumber:number;
+let myBoolean:boolean;
+let myAny:any;
+
+// let strArray:string[];
+// let numArray:number[];
+// let boolArray:boolean[];
+// let anyArray:any[];
+//or
+let strArray:Array<string>;
+let numArray:Array<number>;
+let boolArray:Array<boolean>;
+let anyArray:Array<any>;
+
+let myVoid: void = undefined;
+let myNull: null = null;
+let myUndefined: undefined = undefined;
+
 
 myString="Hello TypeScript Dev";
+myNumber=9;
+myBoolean=true;
+myAny='1'+2;
+
+strArray=['Type','Script'];
+numArray=[10,20];
+boolArray=[true,false];
+anyArray=['number',9,true];
+
+
+
 
 console.log(myString);
+console.log(myNumber);
+console.log(myBoolean);
+console.log(myAny);
+
+console.log(strArray);
+console.log(numArray);
+console.log(boolArray);
+console.log(anyArray);
