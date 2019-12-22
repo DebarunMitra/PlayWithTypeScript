f7c50e8ff93b9b2cdeab25557ed69fdae74470ce
DebarunMitra <debarun96mitra@gmail.com>
2019-12-22 15:14:33 +0530

    classes

---
+++ ProjectOne/js/classes.ts
@@ -0,0 +1,38 @@
+class User{
+  name:string;
+  email:string;
+  age:number;
+  constructor(name:string, email:string, age:number){
+    this.name=name;
+    this.email=email;
+    this.age=age;
+
+    console.log('User Created: '+this.name);
+  }
+
+  register(){
+    console.log(this.name+' is registered');
+  }
+
+  payInvoice(){
+    console.log(this.name+' Payed Invoice');
+  }
+}
+
+class Member extends User{
+  id:number;
+
+  constructor(id:number,name:string,email: string, age:number){
+    super(name,email,age);
+      this.id=id;
+  }
+
+  payInvoice(){
+    super.payInvoice()
+  }
+}
+
+let user=new User('Debarun','deba@gmail.com',23);
+
+let mitra: User= new Member(101,'Mitra','mitra@gmail.com',33);
+mitra.payInvoice();
